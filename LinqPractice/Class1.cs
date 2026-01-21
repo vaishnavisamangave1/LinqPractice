19192eddf6ca3fb629a3480d88e5a689859ddac0
vaishnavisamangave1 <vaishnavisamangave0211@gmail.com>
2026-01-21 20:13:56 +0530

    1.modiied class.cs

---
+++ LinqPractice/Class1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqPractice
+{
+    public delegate int Mydel(int x,int y);
+    public delegate string Mydel1(string x);
+    public class Class1
+
+    {
+        //1. Define a custom delegate that takes two integers and returns their difference. Write code to use it.
+        public static int  Diff(int x,int y)
+        {
+          return x - y;
+        }
+        //2. Create a method Calculate that accepts a delegate and two integers, then executes the delegate.
+        public static int Calculate(Mydel m, int x, int y)
+        {
+            return m(x, y);
+        }
+        public static int Add(int x, int y) => x + y;
+        public static int Sub(int x,int y) => x - y;
+        public static int Mul(int x,int y) => x * y;
+
+        public static int Div(int x, int y) => x / y;
+
+        public static string UpperCase1(string s) => s.ToUpper();
+        public static string LowerCase1(string s) => s.ToLower();
+
+        public static string RevCase1(string s) => new string(s.Reverse().ToArray());
+
+
+        public static string StringOps(Mydel1 m, string input)
+        {
+            return m(input);
+        }
+
+
+        static void Main()
+        {
+            //Class1 c=new Class1();
+            //Mydel m = c.Diff;
+            //int result = m(8, 2);
+            //Console.WriteLine(result);
+
+            //int diff1 = Calculate(Diff, 5, 3);
+            //int diff1 = c.Calculate(Diff, 5, 3);
+            //Console.WriteLine(diff1);
+
+            //int result = Calculate(Add, 5, 8);
+            //Console.WriteLine(result);
+
+            //Mydel op;
+
+            //Console.WriteLine("Choose Operation:");
+            //Console.WriteLine("1. Addition");
+            //Console.WriteLine("2. Subtraction");
+            //Console.WriteLine("3. Multiplication");
+            //Console.WriteLine("4. Division");
+
+            //int choice = int.Parse(Console.ReadLine());
+
+            //switch (choice)
+            //{
+            //    case 1:
+            //        op = Add;
+            //        break;
+
+            //    case 2:
+            //        op = Sub;
+            //        break;
+
+            //    case 3:
+            //        op = Mul;
+            //        break;
+
+            //    case 4:
+            //        op = Div;
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("Invalid choice");
+            //        return;
+            //}
+
+            //Console.Write("Enter first number: ");
+            //int n1 = int.Parse(Console.ReadLine());
+
+            //Console.Write("Enter second number: ");
+            //int n2 = int.Parse(Console.ReadLine());
+
+            //double result = op(n1, n2);
+
+            //Console.WriteLine("Result = " + result);
+
+
+            // 4.Implement a delegate to handle string operations(e.g., uppercase, lowercase, reverse).
+
+            string text = "Hello world";
+
+            string  upper= StringOps(UpperCase1, text);
+
+            Console.WriteLine(upper);
+
+            string lower=StringOps(LowerCase1, text);
+            Console.WriteLine(  lower);
+
+            string rev=StringOps(RevCase1, text);
+            Console.WriteLine(rev);
+
+
+
+
+
+
+
+        }
+    }
+}
--- LinqPractice/Program.cs
+++ LinqPractice/Program.cs
@@ -1,42 +1,92 @@
-﻿namespace LinqPractice;
+﻿//using System;
+//using System.ComponentModel;
+//using System.Security.Cryptography.X509Certificates;
+//using System.Xml.Linq;
+//using static System.Runtime.InteropServices.JavaScript.JSType;
 
-public class LinqExamples
-{
+//namespace LinqPractice;
 
-    public static void Main(string[] args)
-    {
 
-        int[] numbers = { 1, 2, 3, 4, 5, 6, 7 };
+//public class LinqExamples
+//{
+//    public delegate int Mydel(int x, int y);
 
-        //numbers.ToList().ForEach(num => Console.WriteLine(num));
+//    public static void Main(string[] args)
+//    {
 
-        //foreach (var num in numbers)
-        //{
-        //    Console.WriteLine(num);
-        //}
+//        int[] numbers = { 1, 2, 3, 4, 5, 6, 7 };
 
-        //var res= numbers.Select(num =>num).ToArray();
-        // foreach (var i in res)
-        // {
+//        //numbers.ToList().ForEach(num => Console.WriteLine(num));
 
-        //     Console.WriteLine(i);
-        // }
+//        //foreach (var num in numbers)
+//        //{
+//        //    Console.WriteLine(num);
+//        //}
 
-        //int sum = numbers.Sum();
-        //Console.WriteLine(sum);
+//        //var res= numbers.Select(num =>num).ToArray();
+//        // foreach (var i in res)
+//        // {
 
-        var even = numbers.Where(x => x % 2 == 0).ToArray();
-        foreach (var number in even)
-        {
-            Console.WriteLine(number);
-        }
+//        //     Console.WriteLine(i);
+//        // }
 
-        var evenSum = numbers.Where(x => x % 2 == 0).Sum();
-        //foreach(var number in evenSum)
-        //{
-        //    Console.WriteLine(number);
-        //}
-       
+//        //int sum = numbers.Sum();
+//        //Console.WriteLine(sum);
 
-    }
-}
+//        //var even = numbers.Where(x => x % 2 == 0).ToArray();
+//        //foreach (var number in even)
+//        //{
+//        //    Console.WriteLine(number);
+//        //}
+
+//        //var evenSum = numbers.Where(x => x % 2 == 0).Sum();
+//        //Console.WriteLine(evenSum);
+
+//        //var oddsum = numbers.Where(x => x % 2 != 0).Sum();
+//        //Console.WriteLine(oddsum);
+//        //foreach(var number in evenSum)
+//        //{
+//        //    Console.WriteLine(number);
+//        //}
+//        //var oddCount = numbers.Where(x => x % 2 != 0).Count();
+//        //Console.WriteLine(oddCount);
+
+//        //var evenCount=numbers.Where(x=> x %2==0).Count();
+//        //Console.WriteLine(evenCount);
+
+//        // var sortEle = numbers.OrderByDescending(x => x);
+
+//        // foreach(var ele in sortEle) Console.WriteLine(ele);
+//        // //        Sort an array in ascending order.
+//        //  var sortele1 = numbers.OrderBy(x => x).ToArray();
+//        // Console.WriteLine("elements:" + sortele1);
+
+//        // foreach( var sortele2 in sortele1) Console.WriteLine("elements:" + sortele2);
+
+
+//        ////desc
+
+//        // var sortDesc=numbers.OrderByDescending(x => x).ToArray();
+//        // foreach(var sort3 in sortDesc) Console.WriteLine(sort3);
+//        //Search for a given element in an array and print its index.
+
+//        //Remove duplicate elements from an array.
+//        //Find the second largest number in an array.
+
+//        var secondLast = numbers.OrderByDescending(x => x).Skip(2).ToArray();
+//        Console.WriteLine(secondLast);
+
+//        //Merge two arrays into a single array.
+
+
+
+
+
+
+
+
+
+
+
+//    }
+//}
